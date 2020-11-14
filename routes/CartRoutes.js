5be3bd8b6f8c588afc680c49f9ffbfe896dc49c0
bagusrizkis <bagus.tho@gmail.com>
2020-11-14 07:56:50 +0700

    1. Add Cart Controller 2. Passed all Test (minimum test) 3. Modified Authentication untuk customer 4. Mod Test 5. Change datatype phone_number to String

--- routes/CartRoutes.js
+++ routes/CartRoutes.js
@@ -1,6 +1,12 @@
 const route = require('express').Router()
 const { CartController } = require('../controllers')
 
+route.get('/', CartController.getAll)
+route.post('/', CartController.addCart)
+route.put('/:id', CartController.editCart)
+route.patch('/:id', CartController.setPayment)
+route.delete('/', CartController.bulkdeleteCart)
+
 
 
 
