ea7b2634d377b1a982a071a5f10a6b168006dbb5
midham14 <mohammadidham14@gmail.com>
2020-11-16 19:06:10 +0700

    fix & debug testing

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -6,6 +6,7 @@ const { Authentication } = require('../middlewares/Authentication')
 route.post('/customerlogin', UserController.customerLoginHandler)
 route.post('/login', UserController.adminLoginHandler)
 
+route.get('/getkasir', Authentication, UserController.getKasirHandler)
 route.post('/addkasir',Authentication, UserController.addKasirHandler)
 route.patch('/editkasir/:id',Authentication, UserController.editKasirHandler)
 route.delete('/deletekasir/:id',Authentication, UserController.deleteKasirHandler)
